c41130b64612c7ea79c45f62c487e1f6873a9749
Maram Qdah <maram.m.qdah@gmail.com>
2021-09-22 19:14:04 -0400

    add play,js

--- play.js
+++ play.js
@@ -1,27 +1,7 @@
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133',// IP address here,
-    port: 50542,// PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-conn.on("connect", () => {
-  // code that does something when the connection is first established
-  console.log("bla bla bla");
-});
-
-  return conn;
-
-  
-};
-
+const {conn,host,port}=require("./client.js");
 
 
 
 console.log("Connecting ...");
-connect();
+//connect();
